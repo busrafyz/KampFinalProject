cc504d95bb3a9cb2c526d21fbae37045e3838b33
busrafyz <78544700+busrafyz@users.noreply.github.com>
2021-10-08 20:58:21 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+                new Product{ProductId=1, CategoryId=1, ProductName="bardak", UnitPrice=15, UnitsInStock=10},
+                new Product{ProductId=2, CategoryId=1, ProductName="tabak", UnitPrice=4, UnitsInStock=2},
+                new Product{ProductId=3, CategoryId=2, ProductName="kupa", UnitPrice=7, UnitsInStock=8},
+                new Product{ProductId=4, CategoryId=2, ProductName="termos", UnitPrice=5, UnitsInStock=12}
+
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        //LINQ - Language Integrated Query - foreach yerine 
+        public void Delete(Product product)
+        {
+            Product productToDelete;
+
+            productToDelete = _products.SingleOrDefault(p=> p.ProductId == product.ProductId);
+
+            _products.Remove(productToDelete);
+            
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate;
+            productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+
+        }
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //public bu class'a diğer katmanlar da ulaşabilsin demek
+    //internal class'ın default özelliği. internal'da sadece bulunduğu katman erişebiliyor bu class'a
+    public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+
+    }
+}
